85a3299f0ffa92f00022bb3f2a5f1987b6ed678d
Ian McBurnie <ianmcburnie@hotmail.com>
2017-03-23 14:18:12 -0700

    Fixed path in karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/bundle-module.js': 'coverage'
+      'test/static/bundle-module.js': 'coverage'
     },
 
     // test results reporter to use
